881389e431e973a89728e6dce83a5c3e0b4a27d5
xhyue <xhyue_0209@sina.com>
2019-04-01 16:43:12 +0800

    change settings, add apps, create database

--- aidorm/settings.py
+++ aidorm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8f#vnl1p*ju5siq$+&y&hw+j2lnnv^kl&q8z(%3ofgvrs9)lbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dormitory',
+    'student',
+    'usercenter',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'aidorm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aidorm_db',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'POST': 3306,
+        'PASSWORD': '123456',
     }
 }
 
@@ -103,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
