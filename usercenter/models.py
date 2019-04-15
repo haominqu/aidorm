12990742e87511e5ff4aa61a6b303e078531022e
tony <quhm@tedu.cn>
2019-04-15 16:30:40 +0800

    token

--- usercenter/models.py
+++ usercenter/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 MROLE_CHIOCES = (
     (0, "超级管理员"),
     (1, "学校管理员"),
-    (2, "宿舍管理员"),
-    (3, "游客")
+    (2, "基础设施管理"),
+    (3, "导员"),
+    (4, "宿舍管理员"),
 )
 
-class UserInfo(models.Model):
-    username = models.CharField(verbose_name='用户名', max_length=30)
-    password = models.CharField(verbose_name='密码', max_length=200)
+class UserInfo(AbstractUser):
     role = models.IntegerField(verbose_name="角色", choices=MROLE_CHIOCES,default=3)
 
     def __str__(self):
--- usercenter/views.py
+++ usercenter/views.py
@@ -9,7 +9,7 @@ from rest_framework_jwt.utils import jwt_decode_handler
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse,request
 from django.contrib.auth.hashers import make_password,check_password
-
+from django.contrib.auth import login,authenticate
 
 # selfproject
 from .serializers import *
@@ -19,13 +19,20 @@ from .models import *
 # base
 import logging
 import json
-
+import jwt
 
 # Create your views here.
 class UserLogin(APIView):
 
     def get(self,request):
-        pass
+        # token = request.META.get("HTTP_AUTHORIZATION").split(' ')
+        # tokenstr = str.encode(token[2])
+        # a = jwt.decode(tokenstr, 'secret_key', algorithms=['HS256'])
+        token = request.META.get("HTTP_AUTHORIZATION").split(' ')
+        trole = jwt_decode_handler(token[2])
+        print(trole)
+
+
     def post(self,request):
         username = request.POST.get("username", "")
         password = request.POST.get("password", "")
@@ -35,17 +42,23 @@ class UserLogin(APIView):
             error = "用户名密码不能为空"
             return JsonResponse({"result": result, "data": data, "error": error})
         try:
-            user = UserInfo.objects.get(username=username)
-            userpwd = check_password(password, user.userpwd)
-            if userpwd:
+            user = authenticate(username=username,password=password)
+            if user:
+
                 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
                 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
                 logininfo = jwt_payload_handler(user)
                 logininfo['role'] = user.role
                 token = jwt_encode_handler(logininfo)
+                # logininfo={}
+                # logininfo["userid"]=user.id
+                # logininfo["username"]=user.username
+                # logininfo["role"]=user.role
+                # encoded_jwt = jwt.encode(logininfo, 'secret_key',algorithm='HS256')
+                # encoded_jwt = bytes.decode(encoded_jwt)
                 data={}
-                data['co    user'] = user.username
-                data['token'] = token
+                data['user'] = user.username
+                data['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6InFxIiwicm9sZSI6MywiZXhwIjoxNTU1MzQ5ODQwLCJlbWFpbCI6IiJ9.vQZRrKst50GBerLjroHUP-sxed0WjSBgQIApd7saEhI'
                 result = True
                 data = data
                 error = ""
